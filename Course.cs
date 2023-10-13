61071530521b3efb0ed91d19766d2698ebb7f4a6
thanhdnh <myhoangthanh@yahoo.com>
2023-10-13 10:23:33 +0700

    OK

---
+++ Course.cs
@@ -0,0 +1,8 @@
+class Course{
+    public string name;
+    public float point;
+    public Course(string name, float point){
+        this.name = name;
+        this.point = point;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿class Program
+﻿using System.Collections;
+using System.Collections.Generic;
+
+class Program
 {
     static Array createMatrix(int[] lengths, int[] lowerbounds){
         Array matrix = Array.CreateInstance(typeof(Single), 
@@ -24,19 +27,46 @@
                 Console.Write("{0, 15}", x.GetValue(i, j));
             Console.WriteLine();
         }
+        System.Console.WriteLine("\n====\n");
+    }
+    static Array createSparseMatrix(Array x){
+        Array c = (Array)x.Clone();
+        for(int i=c.GetLowerBound(0); i<=c.GetUpperBound(0); i++)
+            for(int j=c.GetLowerBound(1); j<=c.GetUpperBound(1);j++)
+                if(i!=j)
+                    c.SetValue(0f, i, j);
+        return c;
     }
     public static void Main(string[] args)
     {
         Console.Clear();
 
-        Array a = createMatrix(new int[]{2, 3}, new int[]{1, 1});
-        Array b = createMatrix(new int[]{2, 3}, new int[]{1, 1});
+        /*Array a = createMatrix(new int[]{4, 4}, new int[]{1, 1});
+        Array b = createMatrix(new int[]{4, 4}, new int[]{1, 1});
         printMatrix(a);
-        System.Console.WriteLine("----");
         printMatrix(b);
-        System.Console.WriteLine("----");
+        
         Array sum = addMatrices(a, b);
         printMatrix(sum);
+
+        printMatrix(createSparseMatrix(a));*/
+
+        /*List<float> list = new List<float>();
+        list.Add(1.2f);
+        list.AddRange(new float[]{2f, 3f, 5f});
+        List<float>.Enumerator en = list.GetEnumerator();
+        while (en.MoveNext())
+            Console.WriteLine(en.Current);
+
+
+        ArrayList arl = new ArrayList();
+        arl.Add(3); arl.Add(5);
+        float s = (int)arl[0] + (int)arl[1];
+        System.Console.WriteLine(s);*/
+
+        Array course = Array.CreateInstance(typeof(Course), 5);
+        course.SetValue(new Course("DSA", 7.4f), 0);
+        course.SetValue(new Course("OOP", 7.3f), 1);
 
         Console.ReadKey();
     }
