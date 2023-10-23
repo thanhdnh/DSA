31ae8b7912ddc3e8d6e253ac5a48004ccd40ff41
thanhdnh <myhoangthanh@yahoo.com>
2023-10-23 10:02:26 +0700

    OK

--- Program.cs
+++ Program.cs
@@ -61,18 +61,127 @@
         current = current.link;
       }
     }
+    //Exercise
+    public int FindMax(){
+      Node current = header;
+      int max = int.Parse(current.link.element.ToString());
+      current = current.link;
+      while(current.link!=null){
+        if(int.Parse(current.link.element.ToString())>max){
+          max = int.Parse(current.link.element.ToString());
+        }
+        current=current.link;
+      }
+      return max;
+    }
+    public double Average(){
+      Node current = header;
+      int count=1;
+      current = current.link;
+      double sum=double.Parse(current.element.ToString());
+      while(current.link!=null)
+      {
+          double a = double.Parse(current.link.element.ToString());
+          sum+=a;
+          count++;
+          current=current.link;
+      }
+      return sum/count;
+    }
+  }
+  public class Node2
+  {
+    public object element;
+    public Node2 flink, blink;
+    public Node2()
+    {
+      element = null;
+      flink = blink = null;
+    }
+    public Node2(object element)
+    {
+      this.element = element;
+      flink = blink = null;
+    }
+  }
+
+  public class DoubleLinkedList
+  {
+    public Node2 header;
+    public DoubleLinkedList()
+    {
+      header = new Node2("Header");
+    }
+    private Node2 Find(object element)
+    {
+      Node2 current = new Node2();
+      current = header;
+      while (current.element != element)
+      {
+        current = current.flink;
+      }
+      return current;
+    }
+    public void Insert(object newelement, object afterelement)
+    {
+      Node2 current = new Node2();
+      Node2 newnode = new Node2(newelement);
+      current = Find(afterelement);
+      newnode.flink = current.flink;
+      newnode.blink = current;
+      current.flink = newnode;
+    }
+    public void Remove(object element)
+    {
+      Node2 current = Find(element);
+      if (current.flink != null)
+      {
+        current.blink.flink = current.flink;
+        current.flink.blink = current.blink;
+        current.flink = null;
+        current.blink = null;
+      }
+    }
+    private Node2 FindLast()
+    {
+      Node2 current = new Node2();
+      current = header;
+      while (!(current.flink == null))
+        current = current.flink;
+      return current;
+    }
+    public void Print()
+    {
+      Node2 current = new Node2();
+      current = FindLast();
+      while (!(current.blink == null))
+      {
+        Console.WriteLine(current.element);
+        current = current.blink;
+      }
+    }
   }
   static void Main()
   {
     Console.Clear();
 
-    LinkedList list = new LinkedList();
-    list.Insert("41", "Header");
-    list.Insert("15", "41");
-    list.Insert("12", "15");
-    list.Insert("19", "12");
-    list.Insert("21", "19");
+    /*LinkedList list = new LinkedList();
+    list.Insert("4", "Header");
+    list.Insert("1", "4");
+    list.Insert("9", "1");
+    list.Insert("2", "9");
+    list.Insert("3", "2");
     list.Print();
+    Console.WriteLine("Max: {0}", list.FindMax());
+    Console.WriteLine("Average: {0}", list.Average());*/
+
+    DoubleLinkedList dlist = new DoubleLinkedList();
+    dlist.Insert("62", "Header");
+    dlist.Insert("45", "62");
+    dlist.Insert("11", "45");
+    dlist.Insert("40", "11");
+    dlist.Insert("69", "40");
+    dlist.Print();
     
     Console.ReadLine();
   }
